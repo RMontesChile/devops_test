cecb2c71c0932ffe286332ca41a12d5999f40406
RMontesChile <rmont003@gmail.com>
2021-11-01 18:57:39 -0700

    tes junit

---
+++ TestSuma.java
@@ -0,0 +1,21 @@
+package com.devoptest;
+
+import junit.framework.TestCase;
+
+public class TestDeSuma extends TestCase {
+   protected void setUp() throws Exception {
+      super.setUp();
+   }
+
+   protected void tearDown() throws Exception {
+      super.tearDown();
+   }
+
+   public final void testGetSuma() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   public final void testIncrementa() {
+      fail("Not yet implemented"); // TODO
+   }
+}
