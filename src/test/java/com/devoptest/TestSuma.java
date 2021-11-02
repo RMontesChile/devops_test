0c01963da7471daec45616d42afd8fe8e8d162d7
RMontesChile <rmont003@gmail.com>
2021-11-01 20:05:30 -0700

    5 test

--- src/test/java/com/devoptest/TestSuma.java
+++ src/test/java/com/devoptest/TestSuma.java
@@ -12,14 +12,31 @@ public class TestSuma extends TestCase{
 	      super.tearDown();
 	   }
 
-	   public final void testGetSuma() {
+	   public final void testGetSuma1() {
 		   Suma suma = new Suma();
 		   double resultado = suma.getSuma(1.0, 1.0);
 		   assertEquals(2.0, resultado);
 	   }
 
-	   public final void testIncrementa() {
-	      fail("Not yet implemented"); // TODO
+	   public final void testGetSuma2() {
+		   Suma suma = new Suma();
+		   double resultado = suma.getSuma(1.0, 2.0);
+		   assertEquals(3.0, resultado);
+	   }
+	   public final void testGetSuma3() {
+		   Suma suma = new Suma();
+		   double resultado = suma.getSuma(1.0, 3.0);
+		   assertEquals(4.0, resultado);
+	   }
+	   public final void testGetSuma4() {
+		   Suma suma = new Suma();
+		   double resultado = suma.getSuma(1.0, 4.0);
+		   assertEquals(5.0, resultado);
+	   }
+	   public final void testGetSuma5() {
+		   Suma suma = new Suma();
+		   double resultado = suma.getSuma(1.0, 5.0);
+		   assertEquals(6.0, resultado);
 	   }
 
 }
